41e18acc381d82ef375506b78dfd384c56649314
gusaifei <savoygu@gmail.com>
2020-08-06 13:25:07 +0800

    feat(Week09): 第九周实战(字符串算法) 557.反转字符串中的单词 III 于20200805

---
+++ Week09/557.reverse-words-in-a-string-iii.js
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=557 lang=javascript
+ *
+ * [557] 反转字符串中的单词 III
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+  // const split = (s) => {
+  //   let [l, r] = [0, s.length - 1]
+
+  //   const output = []
+  //   while(l <= r) {
+  //     output.push(s[l++])
+  //   }
+    
+  //   return output
+  // }
+  
+  const reverse = (l, L, R) => {
+    while(L < R) {
+      [l[L++], l[R--]] = [l[R], l[L]]
+    }
+  }
+
+  const reverseEachWord = (l) => {
+    n = l.length
+    let start = end = 0
+
+    while(start < n) {
+      while(end < n && l[end] !== ' ') end++
+      reverse(l, start, end - 1)
+      start = ++end
+    }
+  }
+
+
+  // const l = split(s)
+  const l = s.split('')
+  reverseEachWord(l)
+
+  return l.join('')
+  
+  // 1. 利用 API
+  // return s.split(' ').map(v => v.split('').reverse().join('')).join(' ')
+};
+// @lc code=end
+
