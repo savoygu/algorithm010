2656476717f3572b34c98cbce284bf889707fa77
gusaifei <savoygu@gmail.com>
2020-08-06 13:04:40 +0800

    feat(Week09): 第九周实战(字符串算法) 151.翻转字符串里的单词 于20200805

---
+++ Week09/151.reverse-words-in-a-string.js
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=151 lang=javascript
+ *
+ * [151] 翻转字符串里的单词
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+
+  // 2. 手动实现
+  const trim = (s) => {
+    let [l, r] = [0, s.length -1]
+
+    while(l <=r && s[l] === ' ') l++ // 去除左边空格
+    while(l <=r && s[r] === ' ') r-- // 去除右边空格
+
+    const output = []
+
+    while(l <= r) {
+      if (s[l] !== ' ') {
+        output.push(s[l])
+      } else if (output[output.length - 1] !== ' ') {
+        output.push(s[l])
+      }
+      l++
+    }
+
+    return output
+  }
+
+  const reverse = (l, L, R) => { // 反转数组元素
+    while(L < R) {
+      [l[L++], l[R--]] = [l[R], l[L]]
+    }
+  }
+
+  const reverseEachWord = (l) => {
+    n = l.length 
+    let start = end = 0
+
+    while(start < n) {
+      while(end < n && l[end] !== ' ') end++ // 找到一个单词下标边界 [start, end - 1]
+      reverse(l, start, end - 1) // 反转单词
+      start = end = end + 1 // 跳过空格，找下一个单词
+    }
+  }
+
+  const l = trim(s)
+  reverse(l, 0, l.length - 1)
+  reverseEachWord(l)
+
+  return l.join('')
+  
+
+  // 1. API
+  // return s.split(' ').filter(v => v !== '').reverse().join(' ')
+  // return s.split(/\s+/).reverse().join(' ')
+  
+};
+// @lc code=end
+
