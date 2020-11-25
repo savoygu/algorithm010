1e6f6bfd5f93d53f44544496bbf88cb032f5c101
superluckyfir <superluckyfir@qq.com>
2020-11-25 20:05:32 +0800

    feat(Week04): 第四周Homework(贪心, 动态规划) 45.跳跃游戏II 于20201125

--- Week04/45.jump-game-ii.js
+++ Week04/45.jump-game-ii.js
@@ -10,6 +10,25 @@
  * @return {number}
  */
 var jump = function(nums) {
+    const n = nums.length
 
+    let count = 0
+    let end = n - 1 // 最后需要到达的位置，一开始是最后一位
+
+    for (let i = n - 2; i >= 0; i--) {
+      let start = i // 可跳到 end 的起点
+
+      while(start >= 0) {
+        if ((end - start) <= nums[start]) { // 可从 start 跳到 end
+          i = start // 记录最后需要到达的点
+        }
+        start--
+      }
+
+      count++
+      end = i // 更新最后需要到达的点
+    }
+
+    return count
 };
 // @lc code=end
