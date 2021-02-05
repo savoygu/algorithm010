eec4c22c7fea35c3042e687b541f70b3a87c2426
superluckyfir <superluckyfir@qq.com>
2021-02-05 14:27:26 +0800

    refactor(Week01): 刷题了 206.反转链表

--- Week01/206.reverse-linked-list.js
+++ Week01/206.reverse-linked-list.js
@@ -27,18 +27,26 @@ var reverseList = function(head) {
    *   空间复杂度: O(1)
    * 
    */
-  let prev = null
-  let cur = next = head
+  // let prev = null
+  // let cur = next = head
 
-  while(cur) {
-    next = cur.next // 移动到当前节点的下一个节点，用于占位，方便后面 cur 移动到该节点
-    cur.next = prev // 把 cur 的 next 指向 prev 完成反转
-    prev = cur // 把 prev 移动到 cur
-    cur = next // 把 cur 移动到 next 
-  }
+  // while(cur) {
+  //   next = cur.next // 移动到当前节点的下一个节点，用于占位，方便后面 cur 移动到该节点
+  //   cur.next = prev // 把 cur 的 next 指向 prev 完成反转
+  //   prev = cur // 把 prev 移动到 cur
+  //   cur = next // 把 cur 移动到 next 
+  // }
 
-  return prev
+  // return prev
+
+  // 参考：https://leetcode-cn.com/problems/reverse-linked-list/solution/dong-hua-yan-shi-206-fan-zhuan-lian-biao-by-user74/
+  // 递归
+  // 递归终止条件：当前为空、下一个节点为空
+  if (head === null || head.next === null) return head
+  const cur = reverseList(head.next) // 这里的 cur 指最后一个节点
+  head.next.next = head // 让 head 的下一个节点的 next 指向 head (反转)
+  head.next = null // 把原来的指向断开
+  return cur // 每层递归函数返回的都是 cur (最后一个节点)
 };
 // @lc code=end
 
-    
